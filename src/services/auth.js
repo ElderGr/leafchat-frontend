93aebbbd345f2c7ebfc49a628bde4e66e1445be4
ElderGr <gbielrs@gmail.com>
2023-08-28 19:32:34 -0300

    initial structure

--- src/services/auth.js
+++
@@ -1,7 +0,0 @@
-export const usuarioAutenticado = () => localStorage.getItem("usuario-leafchat") !== null;
-
-export const parseJwt = () =>{
-    var base64Url = localStorage.getItem("usuario-leafchat").split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    return JSON.parse(window.atob(base64));
-}
